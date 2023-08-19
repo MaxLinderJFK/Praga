1b30b6409ac5062ba76c4bb63af8ad2f9cad0cdc
Yuriy Tonkoikh <134171816+MaxLinderJFK@users.noreply.github.com>
2023-08-19 18:38:04 +0000

    new

--- scripts.js
+++ scripts.js
@@ -1,9 +1,51 @@
+function generateDaysOfYear() {
+    const daysContainer = document.getElementById("daysOfYear");
+    const startDate = new Date("2023-01-01");
+    const endDate = new Date("2024-01-01");
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    for (let date = startDate; date < endDate; date.setDate(date.getDate() + 1)) {
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        const listItem = document.createElement("li");
+        listItem.className = "nav-item";
+
+        const link = document.createElement("a");
+        link.className = "nav-link";
+        link.href = `day_${year}_${month}_${day}.html`;
+        link.textContent = `${day}.${month}.${year}`;
+
+        if (date.getTime() === today.getTime()) {
+            link.className += " active-day";
+        }
+
+        listItem.appendChild(link);
+        daysContainer.appendChild(listItem);
+    }
+}
+
+function startTime() {
+    const tm = new Date();
+    const h = tm.getHours().toString().padStart(2, '0');
+    const m = tm.getMinutes().toString().padStart(2, '0');
+    const s = tm.getSeconds().toString().padStart(2, '0');
+    const day = tm.getDate().toString().padStart(2, '0');
+    const months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+    const month = months[tm.getMonth()];
+    const year = tm.getFullYear();
+    const daysOfWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    const dayOfWeek = daysOfWeek[tm.getDay()];
+    document.getElementById('dateTime').innerHTML = `${day} ${month} ${year} года. ${dayOfWeek} ${h}:${m}:${s}`;
+    setTimeout(startTime, 500);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const tableContainer = document.getElementById('tableContainer');
     const table = document.createElement('table');
     table.border = "1";
 
-    // Создаем thead и заголовки столбцов
     const thead = document.createElement('thead');
     const headers = ["Столбец 1", "Столбец 2", "Столбец 3"];
     const headerRow = document.createElement('tr');
@@ -15,7 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
     thead.appendChild(headerRow);
     table.appendChild(thead);
 
-    // Создаем тело таблицы и заполняем его данными
     const tbody = document.createElement('tbody');
     for (let i = 0; i < 20; i++) {
         const tr = document.createElement('tr');
@@ -28,6 +69,8 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     table.appendChild(tbody);
 
-    // Добавляем таблицу в контейнер
     tableContainer.appendChild(table);
 });
+
+startTime();
+generateDaysOfYear();
